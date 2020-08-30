658e72a22447354a044461e771c070eb215c0980
gabrielnafuzi <gabrielnafuzi@gmail.com>
2020-08-30 12:39:58 -0300

    Scroll animation redone

--- js/modules/scroll-animation.js
+++ js/modules/scroll-animation.js
@@ -3,24 +3,45 @@ export default class ScrollAnimation {
     this.sections = document.querySelectorAll(sections);
     this.halfWindow = window.innerHeight * 0.6;
 
-    this.animateScroll = this.animateScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animateScroll() {
-    this.sections.forEach(section => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = sectionTop - this.halfWindow < 80;
+  // takes the distance of each item in relation
+  // to the top of the site
+  getDistance() {
+    this.distance = [...this.sections].map(section => {
+      const offset = section.offsetTop;
 
-      if (isSectionVisible) {
-        section.classList.add('active');
-      } else if (section.classList.contains('active')) {
-        section.classList.remove('active');
+      return {
+        element: section,
+        offset: Math.floor(offset - this.halfWindow),
+      };
+    });
+  }
+
+  // checks the distance on each object in
+  // relation to the site scroll
+  checkDistance() {
+    this.distance.forEach(item => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('active');
+      } else if (item.element.classList.contains('active')) {
+        item.element.classList.remove('active');
       }
     });
   }
 
   init() {
-    this.animateScroll();
-    window.addEventListener('scroll', this.animateScroll);
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
+    }
+
+    return this;
+  }
+
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
   }
 }
